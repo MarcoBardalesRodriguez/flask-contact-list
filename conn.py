0008f199e7ee62080a2a30cbe5c9307fefe51678
MarcoBardalesRodriguez <marcoantoniobardalesrodriguez@gmail.com>
2022-07-12 13:25:57 -0500

    add connection to clever cloud

--- conn.py
+++ conn.py
@@ -1,6 +1,13 @@
 
 def conn(app):
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'admin'
-    app.config['MYSQL_PASSWORD'] = 'admin'
-    app.config['MYSQL_DB'] = 'flaskcontactapp'
+    app.config['MYSQL_ADDON_HOST'] = 'bms17s3vmfgx4kamuxz7-mysql.services.clever-cloud.com'
+    app.config['MYSQL_ADDON_USER'] = 'u0d9oj6pizkce9bl'
+    app.config['MYSQL_ADDON_PASSWORD'] = 'GdcXWvf7eM4C7s8PWboG'
+    app.config['MYSQL_ADDON_DB'] = 'bms17s3vmfgx4kamuxz7'
+
+# =bms17s3vmfgx4kamuxz7-mysql.services.clever-cloud.com
+# =u0d9oj6pizkce9bl
+# =GdcXWvf7eM4C7s8PWboG
+# =bms17s3vmfgx4kamuxz7
+# MYSQL_ADDON_PORT=3306
+# MYSQL_ADDON_URI=mysql://u0d9oj6pizkce9bl:GdcXWvf7eM4C7s8PWboG@bms17s3vmfgx4kamuxz7-mysql.services.clever-cloud.com:3306/bms17s3vmfgx4kamuxz7
